f79fa4e2f713baaea1141cd1139f6ef119015c60
Claudio Junior <cjnetbr@hotmail.com>
2024-08-08 15:34:39 +0000

    Aula -1_Login

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
-
+from django.contrib import auth
 
 def cadastro(request):
   if request.method == "GET":
@@ -34,3 +34,19 @@ def cadastro(request):
       )
 
       return redirect('/usuarios/logar')
+
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(request, username=username, password=senha)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/empresarios/cadastrar_empresa') # Vai dar erro
+
+        messages.add_message(request, constants.ERROR, 'Usuario ou senha inválidos')
+        return redirect('/usuarios/logar')
